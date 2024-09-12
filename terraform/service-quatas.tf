2237d6934ce671f0a0656a6174655cc0e4625c43
vsubotskyi <vsubotskyi@arturo.ai>
2024-09-12 17:15:20 +0300

    test

--- terraform/service-quatas.tf
+++ terraform/service-quatas.tf
@@ -5,7 +5,7 @@ resource "aws_servicequotas_service_quota" "vpc_per_region" {
 }
 
 resource "aws_servicequotas_service_quota" "vpc_per_region_west" {
-  provider     = "aws.west"
+  provider     = aws.west
   quota_code   = "L-F678F1CE"
   service_code = "vpc"
   value        = 75
