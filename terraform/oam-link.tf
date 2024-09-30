04db9b950664fdea816187dab190ff1582b92215
vsubotskyi <vsubotskyi@arturo.ai>
2024-09-30 17:18:38 +0300

    DEVOPSX-768: tune oam_link

--- terraform/oam-link.tf
+++ terraform/oam-link.tf
@@ -1,5 +1,13 @@
+locals {
+  oam_sink_account_id = "897722673779"
+  oam_sink_arn = "arn:aws:oam:us-east-1:897722673779:sink/177205d6-3266-4809-9fd9-2182aad64efc"
+  is_monitoring_account = data.aws_caller_identity.this.account_id == local.oam_sink_account_id ? 1 : 0
+}
+
 resource "aws_oam_link" "oam_source_link" {
-  sink_identifier = "arn:aws:oam:us-east-1:897722673779:sink/177205d6-3266-4809-9fd9-2182aad64efc"
+  count = local.is_monitoring_account ? 0 : 1
+
+  sink_identifier = local.oam_sink_arn
   label_template  = "$AccountName"
   resource_types  = ["AWS::Logs::LogGroup"]
 }
