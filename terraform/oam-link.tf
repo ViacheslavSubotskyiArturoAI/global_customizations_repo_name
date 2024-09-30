34ead19c8ba4995beab18f2066020b167e9052da
vsubotskyi <vsubotskyi@arturo.ai>
2024-09-30 18:14:45 +0300

    DEVOPSX-768: tune oam_link

--- terraform/oam-link.tf
+++ terraform/oam-link.tf
@@ -1,6 +1,6 @@
 locals {
-  oam_sink_account_id = "897722673779"
-  oam_sink_arn = "arn:aws:oam:us-east-1:897722673779:sink/177205d6-3266-4809-9fd9-2182aad64efc"
+  oam_sink_arn          = "arn:aws:oam:us-east-1:897722673779:sink/177205d6-3266-4809-9fd9-2182aad64efc"
+  oam_sink_account_id   = split(":",local.oam_sink_arn)[4]
   is_monitoring_account = data.aws_caller_identity.this.account_id == local.oam_sink_account_id ? true : false
 }
 
