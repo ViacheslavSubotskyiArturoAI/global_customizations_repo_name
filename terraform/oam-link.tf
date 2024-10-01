040173555b13e47a9e289168e7531337d303a1f1
vsubotskyi <vsubotskyi@arturo.ai>
2024-10-01 14:14:23 +0300

    DEVOPSX-768: tune add oam_link_excluded_accounts_ids

--- terraform/oam-link.tf
+++ terraform/oam-link.tf
@@ -1,12 +1,20 @@
 locals {
-  oam_link_enabled      = true
-  oam_sink_arn          = "arn:aws:oam:us-east-1:897722673779:sink/177205d6-3266-4809-9fd9-2182aad64efc"
-  oam_sink_account_id   = split(":",local.oam_sink_arn)[4]
+  oam_link_enabled = true
+  oam_sink_arn     = "arn:aws:oam:us-east-1:897722673779:sink/177205d6-3266-4809-9fd9-2182aad64efc"
+  oam_link_excluded_accounts_ids = [
+    "676206942355", # AFT-Management
+#    "741448943971", # Audit
+#    "149536487885", # Log Archive
+#    "692859920284", # Identity
+  ]
+
+  oam_sink_account_id   = split(":", local.oam_sink_arn)[4]
   is_monitoring_account = data.aws_caller_identity.this.account_id == local.oam_sink_account_id ? true : false
+  oam_link_excluded     = local.is_monitoring_account || contains(local.oam_link_excluded_accounts_ids, data.aws_caller_identity.this.account_id) ? true : false
 }
 
 resource "aws_oam_link" "this" {
-  count = local.oam_link_enabled && !local.is_monitoring_account ? 1 : 0
+  count = local.oam_link_enabled && !local.oam_link_excluded ? 1 : 0
 
   sink_identifier = local.oam_sink_arn
   label_template  = "$AccountName"
@@ -14,5 +22,5 @@ resource "aws_oam_link" "this" {
 }
 
 output "oam_link_arn" {
-  value = local.oam_link_enabled && !local.is_monitoring_account ? aws_oam_link.this[0].arn : "NA"
+  value = local.oam_link_enabled && !local.oam_link_excluded ? aws_oam_link.this[0].arn : "NA"
 }
